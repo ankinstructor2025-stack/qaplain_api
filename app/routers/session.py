6d5a83c54d771d77b792aea13c989d73ed169a1e
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-07-08 19:48:23 +0900

    Add files via upload

--- app/routers/session.py
+++ app/routers/session.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, Header, HTTPException
+import firebase_admin
+from firebase_admin import auth
 
 router = APIRouter()
 
-class SessionRequest(BaseModel):
-    user_id: str
-
 @router.post("/session")
-def create_session():
-    return {"status": "session ok"}
+def create_session(authorization: str = Header(...)):
+
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid Authorization header")
+
+    id_token = authorization.replace("Bearer ", "")
+
+    decoded_token = auth.verify_id_token(id_token)
+
+    uid = decoded_token["uid"]
+    email = decoded_token.get("email")
+
+    return {
+        "uid": uid,
+        "email": email,
+    }
