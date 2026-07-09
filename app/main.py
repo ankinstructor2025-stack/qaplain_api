b78b94efc51deb9859c3c223ff320860c41ebfe3
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-07-09 19:20:55 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
-from app.routers.session import router as session_router
+
 from app.core.cors import setup_cors
+from app.core.firebase import initialize_firebase
+
+from app.routers.session import router as session_router
+
 
 def create_app() -> FastAPI:
+    # Firebase Admin SDK 初期化
+    initialize_firebase()
+
+    # FastAPI
     app = FastAPI()
+
+    # CORS設定
     setup_cors(app)
 
+    # Router登録
     app.include_router(session_router)
+
     return app
+
 
 app = create_app()
