9c7723515a1ac07a3c59c7726c32c77bf899ac88
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-07-12 14:36:01 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.routers.admin_users import router as admin_users_router
 from app.routers.general_user import router as general_user_router
 from app.routers.tenants import router as tenants_router
 from app.routers.file_types import router as file_types_router
+from app.routers.authentication_methods import router as authentication_methods_router
 
 def create_app() -> FastAPI:
     # Firebase Admin SDK 初期化
@@ -25,6 +26,7 @@ def create_app() -> FastAPI:
     app.include_router(general_user_router)
     app.include_router(tenants_router)
     app.include_router(file_types_router)
+    app.include_router(authentication_methods_router)
 
     return app
 
